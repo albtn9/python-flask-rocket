8d8331a26067498ca7fb03801fbbe32612be09f9
Gustavo Albertine <112908437+albtn9@users.noreply.github.com>
2025-10-25 17:42:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request ,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_login import UserMixin , login_user , LoginManager , login_required, logout_user , current_user
+#from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY']="my_key_123"
